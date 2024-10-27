f4f32a796e32df7e6e3bb6239cbb7953d11161e0
Lizyimin-cpu <111258834+Lizyimin-cpu@users.noreply.github.com>
2024-10-27 22:34:51 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 // Cross-domain configuration
-header("Access-Control-Allow-Origin: *"); // 或者指定来源 "http://localhost:3001"
+header("Access-Control-Allow-Origin: *"); 
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 header("Access-Control-Allow-Credentials: true");
